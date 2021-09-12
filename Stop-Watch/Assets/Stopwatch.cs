6e6775b3b989ac0cb63612467fcf3f0106291de4
DESKTOP-PL3AINM\GOVT ITI WOMENS <peringhatmanu@gmail.com>
2021-09-12 19:47:36 +0530

    Stop Watch

---
+++ Stop-Watch/Assets/Stopwatch.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Stopwatch : MonoBehaviour
+{
+
+    float timer;
+    float seconds;
+    float minutes;
+    float hours;
+
+    bool start;
+
+
+    [SerializeField] Text stopWatchText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = false;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StopWatchCalcul();
+    }
+
+    void StopWatchCalcul(){
+
+        if (start){
+        timer += Time.deltaTime;
+        seconds = (int)(timer % 60);
+        minutes = (int)((timer / 60) % 60);
+        hours = (int)(timer / 3600);
+
+        stopWatchText.text = hours.ToString ("00") + ":" + minutes.ToString ("00") + ":" + seconds.ToString ("00");
+        }
+        
+    }
+
+    public void startTimer(){
+        start = true;
+    }
+
+    public void stopTimer(){
+        start = false;
+    }
+
+    public void resetTimer(){
+        start = false;
+        timer = 0;
+        stopWatchText.text = "00:00:00";
+    }
+}
